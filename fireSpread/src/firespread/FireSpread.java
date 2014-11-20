2319ac1584e9675c21bb6ee563071b57eb4edb2f
kwangitti <ittirid@gmail.com>
2014-11-20 23:01:21 +0700

    ** need to fix autorun step **

--- fireSpread/src/firespread/FireSpread.java
+++ fireSpread/src/firespread/FireSpread.java
@@ -1,189 +1,253 @@
 package firespread;
+
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 public class FireSpread extends JFrame {
-	private static JPanel panel1, panel2, panel3;
-	private static JLabel label1, label2, label3, label4;
-	private static JButton autoPlay, reset, step;
-	private static JComboBox wd, hd;
-	private static JSlider tree, fire;
-	private static String width[] = { "10", "20", "30", "40", "50"};
-	private static String hight[] = { "10", "20", "30", "40", "50"};
-	private static final int MIN = 10;
-	private static final int MAX = 100;
-	private static final int INIT1 = 100;
-	private static final int INIT2 = 70;
-	private static Grid newGrid;
-	private static int w, h, p, t, x, y;
-	private static Timer timer;
-	static boolean nice = true; // Variable for checking button
-	public static void main(String[] args) throws InterruptedException {
-		initialDefaultValue();
-		initialButton();
-		buttonFunction();
-		frameCreate();
-		settingFunction();
-	}
-	// running method of autorun including timer.
-	private static void letStart() {
-		nice = true;// set to true every time user press button
-		ActionListener actListner = new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				if (nice == true) {
-					newGrid.start();
-				}
-			}
-		};
-		Timer timer = new Timer(500, actListner);
-		timer.start();
-	}
-	// Create Frame and re arranged.
-	public static void frameCreate() {
-		JFrame frame = new JFrame();
-		frame.setTitle("Fire_Spread");
-		frame.setSize(850, 700);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		// add panel into the frame
-		frame.add(panel1, BorderLayout.SOUTH);
-		frame.add(newGrid, BorderLayout.CENTER);
-		// add panel for setting.
-		panel2 = new JPanel();
-		panel2.setLayout(new FlowLayout());
-		panel2.setBorder(BorderFactory.createLineBorder(Color.black));
-		panel3 = new JPanel();
-		panel3.setLayout(new GridLayout(8, 1, 30, 10));
-		// initial setting command
-		label1 = new JLabel(); 
-		label2 = new JLabel();
-		label3 = new JLabel();
-		label4 = new JLabel();
-		
-		label1.setText("Width size");
-		label2.setText("Hight size");
-		label3.setText("Burn probabaility ");
-		label4.setText("Tree probabaility");
-		wd = new JComboBox(width);
-		wd.setSelectedIndex(4);
-		hd = new JComboBox(hight);
-		hd.setSelectedIndex(4);
-		tree = new JSlider(JSlider.HORIZONTAL, MIN, MAX, INIT1);
-		tree.setMajorTickSpacing(20);
-		tree.setMinorTickSpacing(1);
-		tree.setPaintTicks(true);
-		tree.setPaintLabels(true);
-		
-		fire = new JSlider(JSlider.HORIZONTAL, MIN, MAX, INIT2);
-		fire.setMajorTickSpacing(20);
-		fire.setMinorTickSpacing(1);
-		fire.setPaintTicks(true);
-		fire.setPaintLabels(true);
-		
-		panel3.add(label1);
-		panel3.add(wd);
-		panel3.add(label2);
-		panel3.add(hd);
-		panel3.add(label3);
-		panel3.add(fire);
-		panel3.add(label4);
-		panel3.add(tree);
-		
-		panel2.add(panel3);
-		frame.add(panel2, BorderLayout.EAST);
-	}
-	// Create buttons into frame.
-	public static void initialButton() {
-		// create panel1
-		panel1 = new JPanel();
-		panel1.setBorder(BorderFactory.createLineBorder(Color.black));
-		// autoplay button
-		autoPlay = new JButton("Auto Play");
-		panel1.add(autoPlay);
-		// step button
-		step = new JButton("Step");
-		panel1.add(step);
-		// reset button
-		reset = new JButton("Reset");
-		panel1.add(reset);
-	}
-	// Create the default value for variable.
-	public static void initialDefaultValue() {
-		w = 50;
-		h = 50;
-		p = 70;
-		t = 100;
-		x = (int) (Math.random() * w);
-		y = (int) (Math.random() * h);
-		newGrid = new Grid(w, h, p, t, x, y);
-	}
-	// Create buttons function when clicked.
-	public static void buttonFunction() {
-		// Auto play animation.
-		autoPlay.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+
+    private static JPanel panel1, panel2, panel3;
+    private static JLabel label1, label2, label3, label4, label5, label6;
+    private static JButton autoPlay, reset, step, pause;
+    private static JComboBox wd, hd;
+    private static JSlider tree, fire, burn;
+    private static String width[] = {"10", "20", "30", "40", "50"};
+    private static String hight[] = {"10", "20", "30", "40", "50"};
+    private static final int MIN = 0;
+    private static final int MAX = 100;
+    private static final int INIT1 = 100;
+    private static final int INIT2 = 50;
+    private static final int INIT3 = 0;
+    private static Grid newGrid;
+    private static int w, h, p, t, x, y, b, count;
+    private static Timer timer;
+    static boolean nice = true; // Variable for checking button
+    static Thread pa;
+
+    public static void main(String[] args) throws InterruptedException {
+        initialDefaultValue();
+        initialButton();
+        buttonFunction();
+        frameCreate();
+        settingFunction();
+    }
+
+    // running method of autorun including timer.
+    private static void letStart() {
+        nice = true;// set to true every time user press button
+        ActionListener actListner = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                if (nice == true) {
+                    newGrid.start();
+                }
+            }
+        };
+
+        Timer timer = new Timer(500, actListner);
+        timer.start();
+    }
+
+    // Create Frame and re arranged.
+    public static void frameCreate() {
+        final JFrame frame = new JFrame();
+        frame.setTitle("Fire_Spread");
+        frame.setSize(850, 700);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // add panel into the frame
+        frame.add(panel1, BorderLayout.SOUTH);
+        frame.add(newGrid, BorderLayout.CENTER);
+
+        // add panel for setting.
+        panel2 = new JPanel();
+        panel2.setLayout(new FlowLayout());
+        panel2.setBorder(BorderFactory.createLineBorder(Color.black));
+        panel3 = new JPanel();
+        panel3.setLayout(new GridLayout(11, 1, 30, 10));
+        // initial setting command
+        label1 = new JLabel();
+        label2 = new JLabel();
+        label3 = new JLabel();
+        label4 = new JLabel();
+        label5 = new JLabel();
+        label6 = new JLabel();
+
+        label1.setText("Width size");
+        label2.setText("Hight size");
+        label3.setText("Catch probabaility ");
+        label4.setText("Tree probabaility");
+        label5.setText("Burn probability");
+        label6.setText("STEP : " + count);
+
+        wd = new JComboBox(width);
+        wd.setSelectedIndex(4);
+        hd = new JComboBox(hight);
+        hd.setSelectedIndex(4);
+
+        tree = new JSlider(JSlider.HORIZONTAL, MIN, MAX, INIT1);
+        tree.setMajorTickSpacing(20);
+        tree.setMinorTickSpacing(1);
+        tree.setPaintTicks(true);
+        tree.setPaintLabels(true);
+
+        fire = new JSlider(JSlider.HORIZONTAL, MIN, MAX, INIT2);
+        fire.setMajorTickSpacing(20);
+        fire.setMinorTickSpacing(1);
+        fire.setPaintTicks(true);
+        fire.setPaintLabels(true);
+
+        burn = new JSlider(JSlider.HORIZONTAL, MIN, MAX, INIT3);
+        burn.setMajorTickSpacing(20);
+        burn.setMinorTickSpacing(1);
+        burn.setPaintTicks(true);
+        burn.setPaintLabels(true);
+
+        panel3.add(label1);
+        panel3.add(wd);
+        panel3.add(label2);
+        panel3.add(hd);
+        panel3.add(label3);
+        panel3.add(fire);
+        panel3.add(label4);
+        panel3.add(tree);
+        panel3.add(label5);
+        panel3.add(burn);
+        panel3.add(label6);
+
+        panel2.add(panel3);
+        frame.add(panel2, BorderLayout.EAST);
+        frame.setVisible(true);
+    }
+
+    // Create buttons into frame.
+    public static void initialButton() {
+        // create panel1
+        panel1 = new JPanel();
+        panel1.setBorder(BorderFactory.createLineBorder(Color.black));
+        // autoplay button
+        autoPlay = new JButton("Auto Play");
+        panel1.add(autoPlay);
+        // step button
+        step = new JButton("Step");
+        panel1.add(step);
+        // stop button
+        pause = new JButton("Pause");
+        panel1.add(pause);
+        // reset button
+        reset = new JButton("Reset");
+        panel1.add(reset);
+
+    }
+
+    // Create the default value for variable.
+    public static void initialDefaultValue() {
+        w = 50;
+        h = 50;
+        p = 70;
+        t = 100;
+        b = 0;
+        count = 0;
+        x = (int) (Math.random() * w);
+        y = (int) (Math.random() * h);
+        newGrid = new Grid(w, h, p, t, x, y, b, count);
+    }
+
+    // Create buttons function when clicked.
+    public static void buttonFunction() {
+        // Auto play animation.
+        autoPlay.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
 //		        newGrid.reset(w, h, p, t);
-				letStart(); // method for autorun
-			}
-		});
-		// ไฟลามเป็น Step
-		step.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				newGrid.start();
-				nice = false;
-			}
-		});
-		// Reset
-		reset.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				nice = false;
-				newGrid.reset(w, h, p, t);
-			}
-		});
-	}
-	public static void settingFunction() {
-		// width get value
-		wd.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JComboBox cb = (JComboBox) e.getSource();
-				w = Integer.parseInt((String) cb.getSelectedItem());
-			}
-		});
-		// high get value
-		hd.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JComboBox cb = (JComboBox) e.getSource();
-				h = Integer.parseInt((String) cb.getSelectedItem());
-			}
-		});
-	tree.addChangeListener(new ChangeListener() {
-		 public void stateChanged(ChangeEvent e){
-			 JSlider source = (JSlider)e.getSource();
-			if (!source.getValueIsAdjusting()) {
-			   t = (int)source.getValue();
-			}
-			 
-		 }
-		
-	});
-	
-	
-	fire.addChangeListener(new ChangeListener() {
-		 public void stateChanged(ChangeEvent e){
-			 JSlider source = (JSlider)e.getSource();
-			if (!source.getValueIsAdjusting()) {
-			   p = (int)source.getValue();
-			}
-			 
-		 }
-		
-	});
-	}
+                letStart(); // method for autorun
+                label6.setText("STEP : " + count);
+
+            }
+        });
+        // ไฟลามเป็น Step
+        step.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                newGrid.start();
+                nice = false;
+
+                if (newGrid.start()) {
+                    count++;
+                    label6.setText("STEP : " + count);
+                }
+            }
+        });
+        // Reset
+        reset.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                nice = false;
+                newGrid.reset(w, h, p, t, b, count);
+                count = 0;
+                label6.setText("STEP : " + count);
+            }
+        });
+
+        // stop
+        pause.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                nice = false;
+                pa.stop();
+            }
+        });
+    }
+
+    public static void settingFunction() {
+        // width get value
+        wd.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JComboBox cb = (JComboBox) e.getSource();
+                w = Integer.parseInt((String) cb.getSelectedItem());
+            }
+        });
+        // high get value
+        hd.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JComboBox cb = (JComboBox) e.getSource();
+                h = Integer.parseInt((String) cb.getSelectedItem());
+            }
+        });
+        tree.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                JSlider source = (JSlider) e.getSource();
+                if (!source.getValueIsAdjusting()) {
+                    t = (int) source.getValue();
+                }
+
+            }
+
+        });
+
+        fire.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                JSlider source = (JSlider) e.getSource();
+                if (!source.getValueIsAdjusting()) {
+                    p = (int) source.getValue();
+                }
+
+            }
+
+        });
+
+        burn.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                JSlider source = (JSlider) e.getSource();
+                if (!source.getValueIsAdjusting()) {
+                    b = (int) source.getValue();
+                }
+
+            }
+
+        });
+    }
 }
--- fireSpread/src/firespread/Grid.java
+++ fireSpread/src/firespread/Grid.java
@@ -7,54 +7,52 @@ import javax.swing.*;
 
 public class Grid extends JPanel {
 
-    int w, h, p, t;
+    int w, h, p, t, b, count;
 
     private Cell cell[][];
     int cellSize;
 
-    public Grid(int w, int h, int p, int t, int x, int y) {
+    public Grid(int w, int h, int p, int t, int x, int y, int b, int count) {
         this.w = w;
         this.h = h;
         this.p = p;
         this.t = t;
-        
+        this.b = b;
+        this.count = count;
+
         cell = new Cell[w][h];
-        for (int i = 0; i < cell.length; i++) {
-            for (int j = 0; j < cell[0].length; j++) {
-                if ( (i == 0 || j == 0) || (i == w-1 || j == h-1)) {
-                    cell[i][j] = new Cell(Cell.EMPTY);
-                }else if (randomTree(t)) {
-                    cell[i][j] = new Cell(Cell.EMPTY);
-                }else{
-                    cell[i][j] = new Cell(Cell.TREE);
-                }
-            }
-        }
+        this.reset(w, h, p, t, b, count);
 //        cell[x][y].setStatus(2);
         //10*10
         if (w == 10 && h == 10) {
             cellSize = 35;
-        cell[5][5].setStatus(2);
-        //20*20
-        } if (w == 20 && h == 20) {
-            cellSize = 25;
-        cell[10][10].setStatus(2);
-        //30*30
-        } if (w == 30 && h == 30) {
-            cellSize = 19;
-        cell[15][15].setStatus(2);
-        //40*40
-        } if (w == 40 && h == 40) {
-            cellSize = 14;
-        cell[20][20].setStatus(2);
-        //50*50
-        } if (w == 50 && h == 50) {
-            cellSize = 12;
-        cell[25][25].setStatus(2);
+            cell[5][5].setStatus(2);
+            //20*20
         }
+        if (w == 20 && h == 20) {
+            cellSize = 25;
+            cell[10][10].setStatus(2);
+            //30*30
+        }
+        if (w == 30 && h == 30) {
+            cellSize = 19;
+            cell[15][15].setStatus(2);
+            //40*40
+        }
+        if (w == 40 && h == 40) {
+            cellSize = 14;
+            cell[20][20].setStatus(2);
+            //50*50
+        }
+        if (w == 50 && h == 50) {
+            cellSize = 12;
+            cell[25][25].setStatus(2);
+        }
+        Update();
     }
 
     public void Update() {
+        validate();
         repaint();
     }
 
@@ -80,13 +78,20 @@ public class Grid extends JPanel {
         }
     }
 
+    public boolean randomBurn(int b) {
+        int rand = (int) (Math.random() * 100);
+        System.out.println(rand);
+
+        return rand < (b);
+    }
+
     public boolean randomFire(int p) {
         int rand = (int) (Math.random() * 100);
         System.out.println(rand);
 
         return rand < (p);
     }
-    
+
     public boolean randomTree(int t) {
         int rand = (int) (Math.random() * 100);
         System.out.println(rand);
@@ -96,13 +101,13 @@ public class Grid extends JPanel {
 
     public boolean start() {
         HashSet<Cell> localFire = new HashSet();
-       
+
         boolean stillBurn = false;;
-        for (int i = 1; i < cell.length - 1; i++) {  
+        for (int i = 1; i < cell.length - 1; i++) {
             for (int j = 1; j < cell[0].length - 1; j++) {
-                
+
                 if (cell[i][j].getStatus() == Cell.BURNING) {
-                    
+
                     if (cell[i - 1][j].getStatus() == Cell.TREE && randomFire(p)) {
                         localFire.add(cell[i - 1][j]);
                         stillBurn = true;
@@ -122,19 +127,18 @@ public class Grid extends JPanel {
                         localFire.add(cell[i][j - 1]);
                         stillBurn = true;
                     }
-                    
+
                     cell[i][j].setStatus(Cell.EMPTY);
                 }
             }
         }
-        
-           Object temp[] = localFire.toArray();
-           
-           for(int i = 0; i < temp.length; i++ ){
-               ((Cell)temp[i]).setStatus(Cell.BURNING);
-           }
-           
-                      
+
+        Object temp[] = localFire.toArray();
+
+        for (int i = 0; i < temp.length; i++) {
+            ((Cell) temp[i]).setStatus(Cell.BURNING);
+        }
+
         this.repaint();
         return stillBurn;
     }
@@ -146,49 +150,59 @@ public class Grid extends JPanel {
             } catch (InterruptedException ex) {
                 break;
             }
+            count++;
         }
     }
-    
-    public void reset(int w, int h, int p, int t) {
-        
-        int x = (int)(Math.random() * w);
-        int y = (int)(Math.random() * h);
+
+    public void reset(int w, int h, int p, int t, int b, int count) {
+
+        int x = (int) (Math.random() * w);
+        int y = (int) (Math.random() * h);
         this.w = w;
         this.h = h;
         this.p = p;
         cell = new Cell[w][h];
         for (int i = 0; i < cell.length; i++) {
             for (int j = 0; j < cell[0].length; j++) {
-                if ( (i == 0 || j == 0) || (i == w-1 || j == h-1)) {
+                if ((i == 0 || j == 0) || (i == w - 1 || j == h - 1)) {
                     cell[i][j] = new Cell(Cell.EMPTY);
-                }else if (randomTree(t)) {
+                } else if (randomTree(t)) {
                     cell[i][j] = new Cell(Cell.EMPTY);
-                }else{
-                    cell[i][j] = new Cell(Cell.TREE);
+                } else {
+                    if(randomBurn(b)) {
+                        cell[i][j] = new Cell(Cell.BURNING);
+                    } else {
+                        cell[i][j] = new Cell(Cell.TREE);
+                    }
                 }
             }
         }
+
 //        cell[x][y].setStatus(2);
         //10*10
         if (w == 10 && h == 10) {
             cellSize = 35;
-        cell[5][5].setStatus(2);
-        //20*20
-        } if (w == 20 && h == 20) {
+            cell[5][5].setStatus(2);
+            //20*20
+        }
+        if (w == 20 && h == 20) {
             cellSize = 25;
-        cell[10][10].setStatus(2);
-        //30*30
-        } if (w == 30 && h == 30) {
+            cell[10][10].setStatus(2);
+            //30*30
+        }
+        if (w == 30 && h == 30) {
             cellSize = 19;
-        cell[15][15].setStatus(2);
-        //40*40
-        } if (w == 40 && h == 40) {
+            cell[15][15].setStatus(2);
+            //40*40
+        }
+        if (w == 40 && h == 40) {
             cellSize = 14;
-        cell[20][20].setStatus(2);
-        //50*50
-        } if (w == 50 && h == 50) {
+            cell[20][20].setStatus(2);
+            //50*50
+        }
+        if (w == 50 && h == 50) {
             cellSize = 12;
-        cell[25][25].setStatus(2);
+            cell[25][25].setStatus(2);
         }
         repaint();
     }
